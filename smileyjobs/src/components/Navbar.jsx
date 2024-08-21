7019807fb4a73096ab3ddf0456b275ef6bc9955d
CommnetEnterprises <155953018+It-SB@users.noreply.github.com>
2024-08-21 15:19:17 +0200

    Update Navbar.jsx

--- smileyjobs/src/components/Navbar.jsx
+++ smileyjobs/src/components/Navbar.jsx
@@ -93,11 +93,15 @@ const Navbar = () => {
             </div>
           ) : (
             <>
-              <Link to="/logins" className="py-2 px-5 border rounded">
+              <Link 
+              // to="https://relevant-alpaca-95.accounts.dev/sign-in?redirect_url=https%3A%2F%2Fsmiley-webapp.vercel.app%2F" 
+              to ='/logins'
+              className="py-2 px-5 border rounded">
                 Log in
               </Link>
               <Link
-                to="https://relevant-alpaca-95.accounts.dev/sign-up?redirect_url=https%3A%2F%2Fsmiley-webapp.vercel.app%2F"
+                // to="https://relevant-alpaca-95.accounts.dev/sign-up?redirect_url=https%3A%2F%2Fsmiley-webapp.vercel.app%2F"
+              to ='/signups'
                 className="bg-blue py-2 px-5 text-white rounded"
               >
                 Sign up
