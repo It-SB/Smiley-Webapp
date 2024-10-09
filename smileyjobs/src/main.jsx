24ff905cd00eddf6dffba3cd8aefd3af55f38027
CommnetEnterprises <155953018+It-SB@users.noreply.github.com>
2024-10-09 09:08:29 +0200

    Sercurity Fix

--- smileyjobs/src/main.jsx
+++ smileyjobs/src/main.jsx
@@ -4,12 +4,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import router from "./router/router.jsx";
-import { ClerkProvider, RedirectToSignIn, SignedIn, SignedOut } from "@clerk/clerk-react";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-react";
 
 // Fetch Clerk publishable key from environment variables
 const clerk_key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-
-console.log(clerk_key);
 
 if (!clerk_key) {
   throw new Error("VITE_CLERK_PUBLISHABLE_KEY is not defined");
@@ -17,6 +20,6 @@ if (!clerk_key) {
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ClerkProvider publishableKey={clerk_key}>
-      <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </ClerkProvider>
 );
