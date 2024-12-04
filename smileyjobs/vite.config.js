62b9b174171ec25243532aeeace40ee9b4f37088
Lettimaks <96027891+lettimaks@users.noreply.github.com>
2024-12-04 14:52:27 +0200

    Update vite.config.js

--- smileyjobs/vite.config.js
+++ smileyjobs/vite.config.js
@@ -1,52 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'], // Add your assets here
-      manifest: {
-        name: 'Smiley Jobs',
-        short_name: 'App',
-        description: 'Your app description',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        scope: '/',
-        start_url: '/',
-        icons: [
-          {
-            src: '/logo.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/logo.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/logo.png',
-            sizes: '192x192',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-        ],
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     rollupOptions: {
-      onLog(level, log, handler) {
-        if (log.cause && log.cause.message === `Can't resolve original location of error.`) {
-          return
-        }
-        handler(level, log)
-      },
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
@@ -55,7 +14,7 @@ export default defineConfig({
         },
       },
     },
-    sourcemap: false, // Ensure sourcemaps are enabled
+    sourcemap: true, // Ensure sourcemaps are enabled
     chunkSizeWarningLimit: 1000,
   },
-});
+})
