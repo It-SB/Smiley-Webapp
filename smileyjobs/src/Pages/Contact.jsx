4860c893de19691c43e3aaad0577cad1e6894c95
Lettimaks <122460609+CommnetEnterprises@users.noreply.github.com>
2025-06-02 11:26:12 +0200

    about and contact updates

--- smileyjobs/src/Pages/Contact.jsx
+++ smileyjobs/src/Pages/Contact.jsx
@@ -1,13 +1,22 @@
+import { lazy, Suspense } from "react";
+
+const AboutIframe = () => (
+  <iframe
+    src="/h_page/contact.html"
+    title="About Page"
+    loading="lazy"
+    width="100%"
+    height="100%"
+    className="border-none"
+  />
+);
+
 const About = () => {
   return (
     <div className="w-full h-screen">
-      <iframe
-        src="/h_page/contact.html"
-        title="About Page"
-        width="100%"
-        height="100%"
-        className="border-none"
-      ></iframe>
+      <Suspense fallback={<div>Loading About Page...</div>}>
+        <AboutIframe />
+      </Suspense>
     </div>
   );
 };
